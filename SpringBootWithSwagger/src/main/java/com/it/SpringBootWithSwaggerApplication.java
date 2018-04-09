88a62e004d789d4068d436473b07eaf660213f72
yuippe <xiao_zz626@126.com>
2018-04-09 22:57:13 +0800

    add Swagger project

---
+++ SpringBootWithSwagger/src/main/java/com/it/SpringBootWithSwaggerApplication.java
@@ -0,0 +1,12 @@
+package com.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithSwaggerApplication.class, args);
+	}
+}
